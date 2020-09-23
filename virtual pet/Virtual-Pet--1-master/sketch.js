c73df67237c5b999317ee574a22d0f97e367f610
AkshaySHebbar <69149733+AkshaySHebbar@users.noreply.github.com>
2020-09-23 15:15:49 +0530

    Add files via upload

--- virtual pet/Virtual-Pet--1-master/sketch.js
+++ virtual pet/Virtual-Pet--1-master/sketch.js
@@ -1,48 +1,52 @@
-var dog,happyDog,database,foodS,foodStock;
+var dog,dogImg,dogImg1;
+var database;
+var foodS,foodStock;
 
-function preload()
-{
-dog.loadImage("images/dogImg.png");
-happydog.loadImage("images/dogImg1.png");
-}
-
-function setup() {
-  createCanvas(500,500);
-  
-  dog=createSprite(250,250);
-  
-}
-
-
-function draw() {  
-  background(46, 139, 87);
-
-  drawSprites();
-  //add styles here
-
-  if(keyWentDown(UP_ARROW)){
-
-    writeStock(foodS);
-    dog.addImage(dogHappy);
+function preload(){
+    dogImg=loadImage("Images/Dog.png");
+ dogImg1=loadImage("Images/happy dog.png");
   }
 
+function setup() {
+  database=firebase.database();
+  createCanvas(500,500);
+
+  dog=createSprite(250,300,150,150);  
+  dog.addImage(dogImg);
+  dog.scale=0.15;
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  textSize(20); 
 }
+
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg1);
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("white");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press up_Arrow Key To Feed Drago Milk!",130,10,300,20);
+}
+
 function readStock(data){
   foodS=data.val();
 }
 
-function writestock(x){
-
+function writeStock(x){
   if(x<=0){
     x=0;
-  }
-  else{
+  }else{
     x=x-1;
-  }
-
+  } 
   database.ref('/').update({
     Food:x
   })
 }
-
-
